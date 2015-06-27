44b4214589c0e7e3c6dc124aed8b1b1ce69b0036
xgao <xgao@wandoujia.com>
2015-06-27 21:03:27 +0800

    resolve

--- library-jni/jni/reverse.h
+++ library-jni/jni/reverse.h
@@ -9,7 +9,7 @@ int reverse(char *file_path_src, char *file_path_desc,
 int demuxing(const char *src_filename, const char *video_dst_filename, const char *audio_dst_filename);
 int mux(const char *filename);
 
-static void video_decode_example(const char *outfilename, const char *filename);
-static void video_encode_example(const char *filename, int codec_id);
-static void audio_decode_example(const char *outfilename, const char *filename);
-static void audio_encode_example(const char *filename);
+void video_decode_example(const char *outfilename, const char *filename);
+void video_encode_example(const char *filename, int codec_id);
+void audio_decode_example(const char *outfilename, const char *filename);
+void audio_encode_example(const char *filename);
